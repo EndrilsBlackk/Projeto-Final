f49666d0e3d17a118d88c09b199fc4a436752005
EndrilsBlackk <nasuscifao9@gmail.com>
2020-11-12 16:13:39 -0300

    okokokokok

--- app/site/paginas/includes/navegador.php
+++ app/site/paginas/includes/navegador.php
@@ -17,7 +17,7 @@
 
         <ul class="navbar-nav">
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="cpanel/assets/paginas/cpanel/rotas.php?pg=cpanel" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <span class="icon-switch"></span>
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
